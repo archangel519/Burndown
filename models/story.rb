3945b864d0f8f3d7387adae104c2b77e237e4fef
Jeff Ray <roommate519@gmail.com>
2012-04-05 23:34:19 -0500

    added more functionality to story model added more testing for story model

--- models/story.rb
+++ models/story.rb
@@ -1,5 +1,14 @@
 require 'active_record'
 
 class Story < ActiveRecord::Base
-  attr
+  attr_accessor :id
+  attr_accessor :name
+  attr_accessor :description
+  attr_accessor :points
+  
+  validates_uniqueness_of :id
+  
+  def points=(value)
+    @points = value.to_i
+  end
 end
--- spec/story_spec.rb
+++ spec/story_spec.rb
@@ -2,19 +2,37 @@ require 'spec_helper'
  
 describe Story do
 
+  before :all do
+    ActiveRecord::Base.establish_connection(
+      :adapter  => "mysql",
+      :host     => "localhost",
+      :username => "burndown",
+      :password => "HappyBunnyFuzzySquirrel",
+      :database => "burndown"
+    )
+    
+    @story = Story.new  
+  end
+  
   describe "#new" do
-      it "returns a new story object" do
-        
-        ActiveRecord::Base.establish_connection(
-          :adapter  => "mysql",
-          :host     => "localhost",
-          :username => "burndown",
-          :password => "HappyBunnyFuzzySquirrel",
-          :database => "burndown"
-        )
-        
-        story = Story.new  
-        story.should be_an_instance_of Story
+      it "returns a new story object" do        
+        @story.should be_an_instance_of Story
+      end
+  end
+  
+  describe "#points=" do
+      context "with a floating point number" do
+          it "sets #points to an integer" do
+            @story.points = 5.1234
+            @story.points.should eql 5
+          end
+      end
+      
+      context 'with nil' do
+        it "sets #points to 0" do
+          @story.points = nil
+          @story.points.should eql 0
+        end
       end
   end
   
